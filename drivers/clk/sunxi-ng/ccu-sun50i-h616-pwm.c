c5edb7884c26ad588f33ac6bd9ee1406e368742e
Richard Genoud <richard.genoud@bootlin.com>
2025-11-04 10:04:12 +0100

    pwmcc: use CLK_DIVIDER_POWER_OF_TWO flag

--- drivers/clk/sunxi-ng/ccu-sun50i-h616-pwm.c
+++ drivers/clk/sunxi-ng/ccu-sun50i-h616-pwm.c
@@ -6,6 +6,7 @@
  */
 
 #include <linux/clk-provider.h>
+#include <linux/clk.h>
 #include <linux/io.h>
 #include <linux/module.h>
 #include <linux/platform_device.h>
@@ -13,50 +14,46 @@
 #include "ccu_common.h"
 
 #include "ccu_div.h"
-#include "ccu_gate.h"
-#include "ccu_mp.h"
-#include "ccu_mult.h"
-#include "ccu_nk.h"
-#include "ccu_nkm.h"
-#include "ccu_nkmp.h"
-#include "ccu_nm.h"
 
 #include "ccu-sun50i-h616-pwm.h"
 
+#define SUNXI_PWMCC(_struct, _name, _parents, _reg, _shift, _width,	\
+		    _muxshift, _muxwidth, _gate, _flags)		\
+	struct ccu_div _struct = {					\
+		.enable		= _gate,				\
+		.div		= _SUNXI_CCU_DIV_FLAGS(_shift, _width,	\
+						       CLK_DIVIDER_POWER_OF_TWO), \
+		.mux	= _SUNXI_CCU_MUX(_muxshift, _muxwidth),		\
+		.common	= {						\
+			.reg		= _reg,				\
+			.hw.init	= CLK_HW_INIT_PARENTS(_name,	\
+							      _parents, \
+							      &ccu_div_ops, \
+							      _flags),	\
+		}							\
+	}
+
 static const char * const pwm_parents[] = { "osc24M", "apb1" };
 
-static struct clk_div_table pwm_div_table[] = {
-	{ .val = 0, .div = 1 },
-	{ .val = 1, .div = 2 },
-	{ .val = 2, .div = 4 },
-	{ .val = 3, .div = 8 },
-	{ .val = 4, .div = 16 },
-	{ .val = 5, .div = 32 },
-	{ .val = 6, .div = 64 },
-	{ .val = 7, .div = 128 },
-	{ .val = 8, .div = 256 },
-	{ /* Sentinel */ },
-};
-
-static SUNXI_CCU_DIV_TABLE_WITH_MUX_GATE(pwm01_clk, "pwm01", pwm_parents, 0x20,
-					 pwm_div_table, 0, 4, /* div_M */
-					 7, 2, /* mux */
-					 4, /* gate */
-					 0); /* flags */
+static SUNXI_PWMCC(pwm01_clk, "pwm01", pwm_parents, 0x20,
+		   0, 4, /* div_M */
+		   7, 2, /* mux */
+		   4,    /* gate */
+		   0);   /* flags */
 
 
-static SUNXI_CCU_DIV_TABLE_WITH_MUX_GATE(pwm23_clk, "pwm23", pwm_parents, 0x24,
-					 pwm_div_table, 0, 4, /* div_M */
-					 7, 2, /* mux */
-					 4, /* gate */
-					 0); /* flags */
+static SUNXI_PWMCC(pwm23_clk, "pwm23", pwm_parents, 0x24,
+		   0, 4, /* div_M */
+		   7, 2, /* mux */
+		   4,    /* gate */
+		   0);   /* flags */
 
 
-static SUNXI_CCU_DIV_TABLE_WITH_MUX_GATE(pwm45_clk, "pwm45", pwm_parents, 0x28,
-					 pwm_div_table, 0, 4, /* div_M */
-					 7, 2, /* mux */
-					 4, /* gate */
-					 0); /* flags */
+static SUNXI_PWMCC(pwm45_clk, "pwm45", pwm_parents, 0x28,
+		    0, 4, /* div_M */
+		    7, 2, /* mux */
+		    4,    /* gate */
+		    0);   /* flags */
 
 
 static struct ccu_common *sun50i_h616_pwmcc_clks[] = {
@@ -67,9 +64,9 @@ static struct ccu_common *sun50i_h616_pwmcc_clks[] = {
 
 static struct clk_hw_onecell_data sun50i_h616_pwmcc_hw_clks = {
 	.hws	= {
-		[CLK_PWM01]		= &pwm01_clk.common.hw,
-		[CLK_PWM23]		= &pwm23_clk.common.hw,
-		[CLK_PWM45]		= &pwm45_clk.common.hw,
+		[CLK_PWM01] = &pwm01_clk.common.hw,
+		[CLK_PWM23] = &pwm23_clk.common.hw,
+		[CLK_PWM45] = &pwm45_clk.common.hw,
 	},
 	.num = CLK_NUMBER,
 };
@@ -83,6 +80,7 @@ static const struct sunxi_ccu_desc sun50i_h616_pwmcc_desc = {
 
 static int sun50i_h616_pwmcc_probe(struct platform_device *pdev)
 {
+	struct clk *clk_bus;
 	void __iomem *reg;
 	int ret;
 
@@ -90,6 +88,11 @@ static int sun50i_h616_pwmcc_probe(struct platform_device *pdev)
 	reg = devm_platform_ioremap_resource(pdev, 0);
 	if (IS_ERR(reg))
 		return PTR_ERR(reg);
+
+	clk_bus = devm_clk_get_enabled(&pdev->dev, "bus");
+	if (IS_ERR(clk_bus))
+		return dev_err_probe(&pdev->dev, PTR_ERR(clk_bus),
+				     "Failed to get bus clock\n");
 
 	ret = devm_sunxi_ccu_probe(&pdev->dev, reg, &sun50i_h616_pwmcc_desc);
 	if (ret)
--- drivers/clk/sunxi-ng/ccu_div.h
+++ drivers/clk/sunxi-ng/ccu_div.h
@@ -84,25 +84,6 @@ struct ccu_div {
 	unsigned int		fixed_post_div;
 };
 
-#define SUNXI_CCU_DIV_TABLE_WITH_MUX_GATE(_struct, _name, _parents, _reg, \
-					  _table, _shift, _width,	\
-					  _muxshift, _muxwidth,		\
-					  _gate, _flags)		\
-	struct ccu_div _struct = {					\
-		.div		= _SUNXI_CCU_DIV_TABLE(_shift, _width,	\
-						       _table),		\
-		.enable		= _gate,				\
-		.mux	= _SUNXI_CCU_MUX(_muxshift, _muxwidth),		\
-		.common	= {						\
-			.reg		= _reg,				\
-			.hw.init	= CLK_HW_INIT_PARENTS(_name,	\
-							      _parents, \
-							      &ccu_div_ops, \
-							      _flags),	\
-		}							\
-	}
-
-
 #define SUNXI_CCU_DIV_TABLE_WITH_GATE(_struct, _name, _parent, _reg,	\
 				      _shift, _width,			\
 				      _table, _gate, _flags)		\
