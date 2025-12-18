02fc8666548a1579a1e2dbeefec499999560d2e0
Richard Genoud <richard.genoud@bootlin.com>
2025-12-18 09:46:45 +0100

    net: stmmac: sun8i: add support for Allwinner H616 EMAC1
    Add support for the Allwinner H616 EMAC1 in the dwmac-sun8i driver.
    
    The secondary EMAC on Allwinner H616 only supports RMII and 10/100Mbps.
    The "internal PHY" on the latest H616 is on a co-packaged AC300 chip,
    and it's actually not really internal:
    It's connected via RMII (PA0-9 GPIOs) and its clock source comes from
    PWM channel 5.
    
    NB: Previously, the co-packaged PHY in H616 SoCs was from an AC200 chip,
    which is configured by i2c.
    
    Signed-off-by: Richard Genoud <richard.genoud@bootlin.com>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-sun8i.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-sun8i.c
@@ -81,6 +81,13 @@ static const struct reg_field sun8i_syscon_reg_field = {
 	.msb = 31,
 };
 
+/* EMAC1 clock register @ 0x34 in the "system control" address range */
+static const struct reg_field sun8i_syscon_reg_field_emac1 = {
+	.reg = 0x34,
+	.lsb = 0,
+	.msb = 31,
+};
+
 /* EMAC clock register @ 0x164 in the CCU address range */
 static const struct reg_field sun8i_ccu_reg_field = {
 	.reg = 0x164,
@@ -139,6 +146,17 @@ static const struct emac_variant emac_variant_h6 = {
 	.support_mii = true,
 	.support_rmii = true,
 	.support_rgmii = true,
+	.rx_delay_max = 31,
+	.tx_delay_max = 7,
+};
+
+static const struct emac_variant emac_variant_h616_1 = {
+	.syscon_field = &sun8i_syscon_reg_field_emac1,
+	/* The "Internal PHY" of H616 is not on the die. It's on the
+	 * co-packaged AC300 chip instead.
+	 */
+	.soc_has_internal_phy = false,
+	.support_rmii = true,
 	.rx_delay_max = 31,
 	.tx_delay_max = 7,
 };
@@ -1306,6 +1324,8 @@ static const struct of_device_id sun8i_dwmac_match[] = {
 		.data = &emac_variant_a64 },
 	{ .compatible = "allwinner,sun50i-h6-emac",
 		.data = &emac_variant_h6 },
+	{ .compatible = "allwinner,sun50i-h616-emac1",
+		.data = &emac_variant_h616_1 },
 	{ }
 };
 MODULE_DEVICE_TABLE(of, sun8i_dwmac_match);
